2941f3b989dcdf7fc892e7b4576bf11123d8a8f1
Day <night-deploy@outlook.com>
2025-02-26 20:10:41 +0100

    Initial commit

--- src/main/java/me/day/tradesandbartering/TradesAndBartering.java
+++ src/main/java/me/day/tradesandbartering/TradesAndBartering.java
@@ -1,10 +1,19 @@
 package me.day.tradesandbartering;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TradesAndBartering implements ModInitializer {
 
+    public static final String MOD_ID = "More Trades & Bartering";
+    public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
+        VillagerTrades.registerVillagerTrades();
+        WanderingTrades.registerWanderingTrades();
+
+        LOGGER.info("Loaded " + MOD_ID);
     }
 }
