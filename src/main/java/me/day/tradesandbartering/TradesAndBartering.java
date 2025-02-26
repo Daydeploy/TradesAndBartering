625bb36effcd1d5647f3b40469846fe2ff2ad246
Day <night-deploy@outlook.com>
2025-02-26 19:53:15 +0100

    Initial commit

---
+++ src/main/java/me/day/tradesandbartering/TradesAndBartering.java
@@ -0,0 +1,10 @@
+package me.day.tradesandbartering;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TradesAndBartering implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/day/tradesandbartering/client/TradesAndBarteringClient.java
@@ -0,0 +1,10 @@
+package me.day.tradesandbartering.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TradesAndBarteringClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
