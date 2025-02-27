6896f563f72d91fe382b5a657fb44a933a38ea37
Day <night-deploy@outlook.com>
2025-02-27 23:56:34 +0100

    Wandering trader trade rebalanced

--- src/main/java/me/day/tradesandbartering/WanderingTrades.java
+++ src/main/java/me/day/tradesandbartering/WanderingTrades.java
@@ -9,26 +9,33 @@ import net.minecraft.village.TradedItem;
 public class WanderingTrades {
     public static void registerWanderingTrades() {
         TradeOfferHelper.registerWanderingTraderOffers(1, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.TURTLE_EGG), 10,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.PITCHER_POD, 1), 10,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.TORCHFLOWER_SEEDS, 1), 10,5,0.05F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.TURTLE_EGG), 10,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.TADPOLE_BUCKET), 4,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.AXOLOTL_BUCKET), 4,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.GOLDEN_APPLE), 8,1,0.0F));
 
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 3), new ItemStack(Items.GOLDEN_APPLE, 1), 10,5,0.05F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 3), new ItemStack(Items.PITCHER_POD), 3,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 3), new ItemStack(Items.TORCHFLOWER_SEEDS), 3,1,0.0F));
 
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 1), new ItemStack(Items.EXPERIENCE_BOTTLE, 4), 10,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 1), new ItemStack(Items.HONEYCOMB, 3), 10,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 1), new ItemStack(Items.HONEY_BOTTLE, 1), 10,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 1), new ItemStack(Items.FEATHER, 5), 10,5,0.05F));
+
+
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.ARMADILLO_SCUTE), 12,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.EXPERIENCE_BOTTLE), 6,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.HONEYCOMB, 3), 12,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.HONEY_BOTTLE), 6,1,0.0F));
+
         });
 
         TradeOfferHelper.registerWanderingTraderOffers(2, factories -> {
-            // factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 64), new ItemStack(Items.TOTEM_OF_UNDYING), 10,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 32), new ItemStack(Items.SNIFFER_EGG, 1), 10,5,0.05F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 32), new ItemStack(Items.HEART_OF_THE_SEA), 1,1,0.0F));
 
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 10), new ItemStack(Items.CREEPER_HEAD, 1), 3,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 10), new ItemStack(Items.ZOMBIE_HEAD, 1), 3,5,0.05F));
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 10), new ItemStack(Items.SKELETON_SKULL, 1), 3,5,0.05F));
-            // factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 10), new ItemStack(Items.WITHER_SKELETON_SKULL), 3,5,0.05F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 12), new ItemStack(Items.CREEPER_HEAD), 1,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 12), new ItemStack(Items.SKELETON_SKULL), 1,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 12), new ItemStack(Items.ZOMBIE_HEAD), 1,1,0.0F));
+
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.PEARLESCENT_FROGLIGHT), 3,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.OCHRE_FROGLIGHT), 3,1,0.0F));
+            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 5), new ItemStack(Items.VERDANT_FROGLIGHT), 3,1,0.0F));
         });
     }
 }
