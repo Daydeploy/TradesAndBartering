10ff50942600820b9cb1a7fde1dee725c84ab90d
Day <night-deploy@outlook.com>
2025-02-27 02:28:13 +0100

    Villager trade rebalanced

--- src/main/java/me/day/tradesandbartering/VillagerTrades.java
+++ src/main/java/me/day/tradesandbartering/VillagerTrades.java
@@ -20,23 +20,15 @@ public class VillagerTrades {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.REDSTONE, 15), new ItemStack(Items.EMERALD), 16, 2,0.05F));
         });
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.ARMORER, 2, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.COPPER_INGOT, 4), new ItemStack(Items.EMERALD), 12, 10,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.ARMORER, 2, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.COPPER_INGOT, 4), new ItemStack(Items.EMERALD), 12, 10,0.05F)));
     }
 
     private static void ButcherTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.COOKED_RABBIT, 5), 16, 5,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.COOKED_RABBIT, 5), 16, 5,0.05F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 4, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.MUTTON, 5), 16, 5,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 4, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.MUTTON, 5), 16, 5,0.05F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 5, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.BEEF, 5), 12, 5,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 5, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.BEEF, 5), 12, 5,0.05F)));
     }
 
     private static void CartographerTrades() {
@@ -55,9 +47,7 @@ public class VillagerTrades {
             factories.add(((entity, random) -> new TradeOffer(new TradedItem(Items.STRING, 32), new ItemStack(Items.EMERALD), 16, 2, 0.05F)));
         });
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 2, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.GUNPOWDER, 16), new ItemStack(Items.EMERALD), 12, 5,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 2, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.GUNPOWDER, 16), new ItemStack(Items.EMERALD), 12, 5,0.05F)));
 
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 3, factories -> {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.DISC_FRAGMENT_5), new ItemStack(Items.EMERALD), 3, 20,0.05F));
@@ -69,9 +59,7 @@ public class VillagerTrades {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 8), new ItemStack(Items.GHAST_TEAR), 12, 15,0.05F));
         });
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 5, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 8), new ItemStack(Items.PHANTOM_MEMBRANE), 12, 30,0.2F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 5, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 8), new ItemStack(Items.PHANTOM_MEMBRANE), 12, 30,0.2F)));
     }
 
     private static void FarmerTrades() {
@@ -81,24 +69,18 @@ public class VillagerTrades {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.SUGAR_CANE, 32), new ItemStack(Items.EMERALD), 16, 10,0.05F));
         });
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.MOSS_BLOCK, 16), new ItemStack(Items.EMERALD), 12, 15,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.MOSS_BLOCK, 16), new ItemStack(Items.EMERALD), 12, 15,0.05F)));
 
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 4, factories -> {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.BROWN_MUSHROOM, 32), new ItemStack(Items.EMERALD), 12, 20,0.05F));
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.RED_MUSHROOM, 32), new ItemStack(Items.EMERALD), 12, 20,0.05F));
         });
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 5, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 10), new ItemStack(Items.GOLDEN_APPLE), 12, 30,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 5, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 10), new ItemStack(Items.GOLDEN_APPLE), 12, 30,0.05F)));
     }
 
     private static void FishermanTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FISHERMAN, 1, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.WATER_BUCKET), 16, 1,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FISHERMAN, 1, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.WATER_BUCKET), 16, 1,0.05F)));
 
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.FISHERMAN, 2, factories -> {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.SEAGRASS, 16), new ItemStack(Items.EMERALD), 16, 10,0.05F));
@@ -112,19 +94,13 @@ public class VillagerTrades {
     }
 
     private static void FletcherTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FLETCHER, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.SPECTRAL_ARROW, 8), 12, 10,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FLETCHER, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.SPECTRAL_ARROW, 8), 12, 10,0.05F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FLETCHER, 5, factories -> {
-            factories.add(new TradeOffers.SellPotionHoldingItemFactory(Items.SPECTRAL_ARROW, 5,Items.TIPPED_ARROW, 5, 1, 12, 30));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FLETCHER, 5, factories -> factories.add(new TradeOffers.SellPotionHoldingItemFactory(Items.SPECTRAL_ARROW, 5,Items.TIPPED_ARROW, 5, 1, 12, 30)));
     }
 
     private static void LeatherWorkerTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LEATHERWORKER, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.BUNDLE), 3, 10,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LEATHERWORKER, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.BUNDLE), 3, 10,0.05F)));
 
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.LEATHERWORKER, 5, factories -> {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 32), new ItemStack(Items.DIAMOND_HORSE_ARMOR), 3, 30,0.2F));
@@ -134,17 +110,11 @@ public class VillagerTrades {
     }
 
     private static void LibrarianTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 1, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.BAMBOO, 48), new ItemStack(Items.EMERALD), 16, 2,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 1, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.BAMBOO, 48), new ItemStack(Items.EMERALD), 16, 2,0.05F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.GLOW_INK_SAC, 4), new ItemStack(Items.EMERALD), 12, 20,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.GLOW_INK_SAC, 4), new ItemStack(Items.EMERALD), 12, 20,0.05F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 5, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 20), new ItemStack(Items.DRAGON_BREATH), 12, 30,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 5, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 20), new ItemStack(Items.DRAGON_BREATH), 12, 30,0.05F)));
     }
 
     private static void MasonTrades() {
@@ -165,19 +135,13 @@ public class VillagerTrades {
             factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.PRISMARINE), 12, 20,0.05F));
         });
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 4, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.PRISMARINE, 8), Optional.of(new TradedItem(Items.EMERALD)), new ItemStack(Items.DARK_PRISMARINE, 8), 16, 10,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 4, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.PRISMARINE, 8), Optional.of(new TradedItem(Items.EMERALD)), new ItemStack(Items.DARK_PRISMARINE, 8), 16, 10,0.05F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 5, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.QUARTZ_BRICKS), 12, 30,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 5, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD), new ItemStack(Items.QUARTZ_BRICKS), 12, 30,0.05F)));
     }
 
     private static void ShepherdTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.SHEPHERD, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 8), new ItemStack(Items.WOLF_ARMOR), 3, 30,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.SHEPHERD, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 8), new ItemStack(Items.WOLF_ARMOR), 3, 30,0.05F)));
     }
 
     private static void ToolSmithTrades() {
@@ -190,23 +154,15 @@ public class VillagerTrades {
 
         });
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.TOOLSMITH, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.IRON_HOE), 3, 10,0.2F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.TOOLSMITH, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.IRON_HOE), 3, 10,0.2F)));
     }
 
     private static void WeaponSmithTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 1, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 1), new ItemStack(Items.SHIELD), 12, 1,0.2F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 1, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 1), new ItemStack(Items.SHIELD), 12, 1,0.2F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 2, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.COPPER_INGOT, 4), new ItemStack(Items.EMERALD), 12, 10,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 2, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.COPPER_INGOT, 4), new ItemStack(Items.EMERALD), 12, 10,0.05F)));
 
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 3, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.FLINT_AND_STEEL), 12, 10,0.05F));
-        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 3, factories -> factories.add((entity, random) -> new TradeOffer(new TradedItem(Items.EMERALD, 2), new ItemStack(Items.FLINT_AND_STEEL), 12, 10,0.05F)));
     }
 
 
