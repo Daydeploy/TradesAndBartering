94ccd79debf9c43475d82934a436f6e81759334d
Day <night-deploy@outlook.com>
2025-06-22 01:16:21 +0200

    Redundant multi-loader template code cleanup

--- neoforge/src/main/java/me/day/tradesandbartering/TradesAndBartering.java
+++ neoforge/src/main/java/me/day/tradesandbartering/TradesAndBartering.java
@@ -1,19 +1,7 @@
 package me.day.tradesandbartering;
 
-
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
-
-@Mod(Constants.MOD_ID)
 public class TradesAndBartering {
 
-    public TradesAndBartering(IEventBus eventBus) {
-        // This method is invoked by the NeoForge mod loader when it is ready
-        // to load your mod. You can access NeoForge and Common code in this
-        // project.
-
-        // Use NeoForge to bootstrap the Common mod.
-        Constants.LOG.info("Hello NeoForge world!");
-        CommonClass.init();
+    public TradesAndBartering() {
     }
 }
