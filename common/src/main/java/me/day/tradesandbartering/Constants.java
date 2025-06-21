939ca05e39d14749553245c8d6aac4217d540993
Day <night-deploy@outlook.com>
2025-06-22 01:04:45 +0200

    Initial commit

---
+++ common/src/main/java/me/day/tradesandbartering/Constants.java
@@ -0,0 +1,11 @@
+package me.day.tradesandbartering;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "tradesandbartering";
+    public static final String MOD_NAME = "TradesAndBartering";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ neoforge/src/main/java/me/day/tradesandbartering/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,23 @@
+package me.day.tradesandbartering.platform;
+
+import me.day.tradesandbartering.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+        return !FMLLoader.isProduction();
+    }
+}
