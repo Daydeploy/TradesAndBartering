fad4bf50dc7e94a4b8181969050da5183b8c9c79
Day <night-deploy@outlook.com>
2025-06-22 22:12:04 +0200

    Add mod description, change mod name and add ForgeModLoader support

---
+++ forge/src/main/java/me/day/tradesandbartering/TradesAndBartering.java
@@ -0,0 +1,11 @@
+package me.day.tradesandbartering;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CommonClass.MOD_ID)
+public class TradesAndBartering {
+
+    public TradesAndBartering() {
+        CommonClass.initialize();
+    }
+}
